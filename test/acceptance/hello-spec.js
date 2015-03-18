33755c720748dfb6bae6138ad5aa3c5f60591082
Ivan Storck <ivanoats@gmail.com>
2015-03-17 17:45:30 -0700

    spike on selenium wd approach

--- test/acceptance/hello-spec.js
+++ test/acceptance/hello-spec.js
@@ -1,33 +1,44 @@
+/* eslint-disable no-unused-expressions */
+require('colors');
 var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should();
 var expect = chai.expect;
 var port = process.env.port || 3000;
-var async = require('async');
+var wd = require('wd');
+
+// enables chai assertion chaining
+chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 
 describe('acceptance test', function() {
+  this.timeout(10000);
+  var browser;
 
-  it('has a browser injected into it', function () {
-    expect(this.browser).to.be.defined;
+  before(function() {
+    browser = wd.promiseChainRemote();
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info.cyan);
+    });
+    browser.on('command', function(eventType, command, response) {
+      console.log(' > ' + eventType.cyan, command, (response || '').grey);
+    });
+    browser.on('http', function(meth, path, data) {
+      console.log(' > ' + meth.magenta, path, (data || '').grey);
+    });
+    return browser.init({browserName:'chrome'});
   });
 
-  it('has title of React Starter', function(done) {
-    var browser = this.browser;
-    async.waterfall([
-      function(cb) {
-        browser.get('http://localhost:' + port);
-      },
-      function(cb) {
-        console.log(browser.title());
-        cb(browser.title());
-      },
-      function(val, cb) {
-        try {
-          expect(val).to.equal('React Starter');
-          cb();
-        } catch(e) {
-          cb(e);
-        }
-      }
-    ], done);
+  beforeEach(function() {
+    return browser.get('http://localhost:' + port);
+  });
 
+  after(function() {
+    return browser.quit();
+  });
+
+  it('has title of React Starter', function() {
+    return browser.title().should.become('React Starter');
   });
 });
